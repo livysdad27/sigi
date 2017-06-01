5d746c611c4b33f495f711aae38388e9c415da6f
livysdad27 <livysdad27@gmail.com>
2017-06-01 18:40:33 +0000

    fixed scoping variable problem

--- public/main.js
+++ public/main.js
@@ -2,8 +2,11 @@
 var socket = io();
 
 var video=document.querySelector('video');
+var rvideo=document.querySelector('rvideo');
 var em = document.getElementById('emitter');
 var msgArea = document.getElementById('msgArea');
+var servers = null;
+var numUsers = window.numUsers = 0;
 
 var constraints = window.constraints = {
   audio: false,
@@ -20,10 +23,14 @@ function onMsg(e){
 
 socket.on('user joined', function(data){
   console.log('user ' + data.socketid + ' joined');
-  msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' joined';
+  msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' joined ' + numUsers + ' users here.';
+  if (data.numUsers == 1){
+    conn.createOffer().then(sendOffer).catch(onCreateSDPError);
+  };
 });
 
 socket.on('user left', function(data){
+  window.numUsers = data.numUsers;
   console.log('user ' + data.socketid + ' left');
   msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' left';
 });
@@ -33,6 +40,15 @@ socket.on('new message', function(data){
   msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' said:' + data.message;
 });
 
+socket.on('offer', function(data){
+  console.log('user ' + data.socketid + ' said ' + data.message);
+  msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' said:' + data.message;
+});
+
+socket.on('answer', function(data){
+  console.log('user ' + data.socketid + ' said ' + data.message);
+  msgArea.innerHTML = msgArea.innerHTML + '<br>' + data.socketid + ' said:' + data.message;
+});
 
 function handleSuccess(stream){
   var videoTracks = stream.getVideoTracks();
@@ -45,3 +61,21 @@ function handleError(error){
 };
 
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+
+conn = new RTCPeerConnection(servers);
+conn.addStream = window.stream; 
+
+function onCreateSDPError(error){
+  console.log('Create SDP Error: ' + error.name);
+};
+
+function sendOffer(SDP){
+  socket.emit('offer', conn.localDescription);
+  console.log('offer made: ' + SDP);
+}
+
+function sendAnswer(SDP){
+  socket.emit('answer', conn.localDescription);
+  console.log('answer made: ' + SDP);
+}
+
